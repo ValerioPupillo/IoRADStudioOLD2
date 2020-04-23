2b0d978859000603411ca88475e79ad53c615881
ValerioPupillo <59449064+ValerioPupillo@users.noreply.github.com>
2020-04-23 18:47:06 +0200

    project_MDADelphi update

--- MDADelphi/formMain.pas
+++ MDADelphi/formMain.pas
@@ -1,17 +1,39 @@
+// ------------------------------------------------------------------------ ||
+// ------------------------------------------------------------------------ ||
+
+// formMain
+// --------
+// Form principale dell'applicazione.
+// Versione 1.0.0 del 23/04/2020
+
+// ------------------------------------------------------------------------ ||
+// ------------------------------------------------------------------------ ||
+
 unit formMain;
 
 interface
 
 uses
-  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+  System.SysUtils,                                                                // alla creazione
+  System.Types,                                                                   // alla creazione
+  System.UITypes,                                                                 // alla creazione
+  System.Classes,                                                                 // alla creazione
+  System.Variants,                                                                // alla creazione
+  FMX.Types,                                                                      // alla creazione
+  FMX.Controls,                                                                   // alla creazione
+  FMX.Forms,                                                                      // alla creazione
+  FMX.Graphics,                                                                   // alla creazione
+  FMX.Dialogs,                                                                    // alla creazione
+  FMX.Menus;                                                                      // inserendo un TMenuBar
 
 type
   TfrmMain = class(TForm)
+    mbMain: TMenuBar;
+
   private
-    { Private declarations }
+
   public
-    { Public declarations }
+
   end;
 
 var
