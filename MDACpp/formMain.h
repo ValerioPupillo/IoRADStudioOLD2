0c89ed202a664d7491f1f14ce5210e71f177dd5f
ValerioPupillo <59449064+ValerioPupillo@users.noreply.github.com>
2020-04-25 18:11:42 +0200

    Creazione Project MDACpp
    This reverts commit b373fcc215f018f6668a272b460c240d233c45e8.

---
+++ MDACpp/formMain.h
@@ -0,0 +1,20 @@
+//---------------------------------------------------------------------------
+
+#ifndef formMainH
+#define formMainH
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <FMX.Controls.hpp>
+#include <FMX.Forms.hpp>
+//---------------------------------------------------------------------------
+class TfrmMain : public TForm
+{
+__published:	// IDE-managed Components
+private:	// User declarations
+public:		// User declarations
+	__fastcall TfrmMain(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TfrmMain *frmMain;
+//---------------------------------------------------------------------------
+#endif
