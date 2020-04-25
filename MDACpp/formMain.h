d457742f1b8148f3e591e83612817a341c9e18b9
ValerioPupillo <59449064+ValerioPupillo@users.noreply.github.com>
2020-04-25 18:22:01 +0200

    Update Project MDACpp

--- MDACpp/formMain.h
+++ MDACpp/formMain.h
@@ -1,20 +1,34 @@
-//---------------------------------------------------------------------------
+// ------------------------------------------------------------------------ ||
+// ------------------------------------------------------------------------ ||
 
-#ifndef formMainH
-#define formMainH
-//---------------------------------------------------------------------------
-#include <System.Classes.hpp>
-#include <FMX.Controls.hpp>
-#include <FMX.Forms.hpp>
-//---------------------------------------------------------------------------
+#ifndef formMainH																																	// alla creazione
+#define formMainH																																	// alla creazione
+#include <System.Classes.hpp>																											// alla creazione
+#include <FMX.Controls.hpp>																												// alla creazione
+#include <FMX.Forms.hpp>                                                          // alla creazione
+#include <FMX.Types.hpp>                                                          // inserendo un qualsiasi TObject
+#include <FMX.Menus.hpp>                                                          // inserendo un TMenuBar
+
+// ------------------------------------------------------------------------ ||
+// ------------------------------------------------------------------------ ||
+
 class TfrmMain : public TForm
 {
-__published:	// IDE-managed Components
-private:	// User declarations
-public:		// User declarations
-	__fastcall TfrmMain(TComponent* Owner);
+	__published:
+		TMenuBar *mbMain;
+
+	private:
+
+	public:
+		__fastcall TfrmMain(TComponent* Owner);
+		__fastcall ~TfrmMain();
 };
-//---------------------------------------------------------------------------
+
+// ------------------------------------------------------------------------ ||
+// ------------------------------------------------------------------------ ||
+
 extern PACKAGE TfrmMain *frmMain;
-//---------------------------------------------------------------------------
 #endif
+
+// ------------------------------------------------------------------------ ||
+// ------------------------------------------------------------------------ ||
